d716e565491794b1a2ce4bece5a5b2b25814a96b
Wayne Buxton <waynebuxton2184@yahoo.com>
2018-03-01 16:33:32 +0000

    Changed to index

--- lib/adventure_novels/cli.rb
+++ lib/adventure_novels/cli.rb
@@ -9,7 +9,7 @@ class AdventureNovels::CLI
   def list_novels
     puts "Top 10 Adventure Novels:"
     page = Nokogiri::HTML(open("https://www.publishersweekly.com/pw/by-topic/industry-news/tip-sheet/article/69690-the-10-best-adventure-novels.html"))
-      puts page.css(".listicle-info").first.css("h3").text
+      puts page.css(".listicle-info")[0].css("h3").text
     #binding.pry
   end
 
@@ -19,9 +19,9 @@ class AdventureNovels::CLI
     page = Nokogiri::HTML(open("https://www.publishersweekly.com/pw/by-topic/industry-news/tip-sheet/article/69690-the-10-best-adventure-novels.html"))
     case input
     when "1"
-      puts  page.css(".listicle-info").first.css('p').text
+      puts  page.css(".listicle-info")[0].css('p').text
     when "2"
-      puts "More info on deal 2..."
+      puts page.css(".listicle-info")[1].css('p').text
     end
   end
 end
