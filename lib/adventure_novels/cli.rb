9c44311cf5cbc42e843c2166e99abc6c7f71fb6c
Wayne Buxton <waynebuxton2184@yahoo.com>
2018-03-01 17:11:16 +0000

    put in list_novels

--- lib/adventure_novels/cli.rb
+++ lib/adventure_novels/cli.rb
@@ -9,8 +9,8 @@ class AdventureNovels::CLI
   def list_novels
     puts "Top 10 Adventure Novels:"
     page = Nokogiri::HTML(open("https://www.publishersweekly.com/pw/by-topic/industry-news/tip-sheet/article/69690-the-10-best-adventure-novels.html"))
-      puts page.css(".listicle-info")[0].css("h3").text
-    #binding.pry
+    title = page.css("div.listicle-item")
+    title.each {|t| puts t.css("h3").text }
   end
 
   def menu
@@ -22,6 +22,22 @@ class AdventureNovels::CLI
       puts  page.css(".listicle-info")[0].css('p').text
     when "2"
       puts page.css(".listicle-info")[1].css('p').text
+    when "3"
+      puts page.css(".listicle-info")[2].css('p').text
+    when "4"
+      puts page.css(".listicle-info")[3].css('p').text
+    when "5"
+      puts page.css(".listicle-info")[4].css('p').text
+    when "6"
+      puts page.css(".listicle-info")[5].css('p').text
+    when "7"
+      puts page.css(".listicle-info")[6].css('p').text
+    when "8"
+      puts page.css(".listicle-info")[7].css('p').text
+    when "9"
+      puts page.css(".listicle-info")[8].css('p').text
+    when "10"
+      puts page.css(".listicle-info")[9].css('p').text
     end
   end
 end
