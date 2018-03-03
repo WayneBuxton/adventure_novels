56382a1cdf6eb3485fedb15eb4437072867742a8
Wayne Buxton <waynebuxton2184@yahoo.com>
2018-03-03 22:41:43 +0000

    attr_accessor

--- lib/adventure_novels/novel.rb
+++ lib/adventure_novels/novel.rb
@@ -1,5 +1,7 @@
 class AdventureNovels::Novel
 
+  attr_accessor :title, :author, :url
+
   def self.all
     page = Nokogiri::HTML(open("https://www.publishersweekly.com/pw/by-topic/industry-news/tip-sheet/article/69690-the-10-best-adventure-novels.html"))
     page.css("div.listicle-item").each {|t| puts t.css("h3").text }
