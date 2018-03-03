d957cde629b27727d193bcd33e7417c5c6a2bc6a
Wayne Buxton <waynebuxton2184@yahoo.com>
2018-03-03 22:39:48 +0000

    novel_1

--- lib/adventure_novels/novel.rb
+++ lib/adventure_novels/novel.rb
@@ -4,4 +4,11 @@ class AdventureNovels::Novel
     page = Nokogiri::HTML(open("https://www.publishersweekly.com/pw/by-topic/industry-news/tip-sheet/article/69690-the-10-best-adventure-novels.html"))
     page.css("div.listicle-item").each {|t| puts t.css("h3").text }
   end
+
+  novel_1 = self.new
+  novel_1.title = "The Odyssey"
+  novel_1.author = "Homer"
+  novel_1.url = "https://www.publishersweekly.com/pw/by-topic/industry-news/tip-sheet/article/69690-the-10-best-adventure-novels.html"
 end
+
+#[novel_1, novel_2, novel_3 etc..]
